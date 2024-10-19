4b7a16f0514279963fc5c1c63d8b9f3af2250a75
Pritam Dey <108470372+Pritam20022002@users.noreply.github.com>
2024-10-20 00:12:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://deypritam7076:FK0NMutswHWPUrtZ@cluster0.nnrh3.mongodb.net/Art-products').then(()=>console.log("DB Connected"));
+    await mongoose.connect('_ADD DATABASE CONNECTION STRING_').then(()=>console.log("DB Connected"));
 }
