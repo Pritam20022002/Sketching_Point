ef66f9bed0676c9ddb7a7601fe6c4c5670b80ce6
Pritam20022002 <deypritam7076@gmail.com>
2024-11-06 19:46:50 +0530

    added frontend

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,85 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+
+import collections_1 from './collections_1.png'
+import collections_2 from './collections_2.png'
+import collections_3 from './collections_3.png'
+import collections_4 from './collections_4.png'
+import collections_5 from './collections_5.png'
+import collections_6 from './collections_6.png'
+import collections_7 from './collections_7.png'
+import collections_8 from './collections_8.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const collections_list = [
+    {
+        collections_name: "Charcoal art",
+        collections_image: collections_1
+    },
+    {
+        collections_name: "Oil paintings",
+        collections_image: collections_2
+    },
+    {
+        collections_name: "Watercolor Art",
+        collections_image: collections_3
+    },
+    {
+        collections_name: "Acrylic paintings",
+        collections_image: collections_4
+    },
+    {
+        collections_name: "Pencil sketches",
+        collections_image: collections_5
+    },
+    {
+        collections_name: "Digital art",
+        collections_image: collections_6
+    },
+    {
+        collections_name: "Pastel art",
+        collections_image: collections_7
+    },
+    {
+        collections_name: "Ink drawings",
+        collections_image: collections_8
+    }]
